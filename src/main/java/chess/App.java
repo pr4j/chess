4ad6fe680694ba7190de35f70518216959f39000
unknown <praj.sagar7@gmail.com>
2018-12-25 01:35:20 +0530

    Stub Game Manager

--- src/main/java/chess/App.java
+++ src/main/java/chess/App.java
@@ -3,12 +3,30 @@
  */
 package chess;
 
+import com.google.common.collect.Maps;
+import manager.GameManager;
+
+import java.util.Map;
+
 public class App {
-    public String getGreeting() {
-        return "Hello world.";
-    }
+    private static Map<Integer, GameManager> gameManagerMap = Maps.newHashMap();
 
     public static void main(String[] args) {
-        System.out.println(new App().getGreeting());
+        App app = new App();
+
+        gameManagerMap.put(0, new GameManager());
+
+        for (int i = 0; i < 100; ++i) {
+            app.play(0,"a1", "b2");
+        }
+    }
+
+    // Public Web API
+    public void play(Integer gameId, String initPos, String finPos) {
+        if (gameManagerMap.containsKey(gameId)) {
+            gameManagerMap.get(gameId).play(initPos, finPos);
+        } else {
+            System.err.println("STUB: Log error: Invalid game ID.");
+        }
     }
 }
---
+++ src/main/java/manager/GameManager.java
@@ -0,0 +1,28 @@
+package manager;
+
+public class GameManager {
+    private Long turnCounter;
+
+    private int turn = 0;
+
+    public GameManager() {
+        //set up the initial board.
+    }
+
+    public void play(String initPos, String finPos) {
+        if (areValidPositions(initPos, finPos)) {
+            updateBoard(initPos, finPos);
+        } else {
+            System.err.println(String.format("STUB: Log error: Invalid positions init=%s fin=%s", initPos, finPos));
+        }
+    }
+
+    private static boolean areValidPositions(String initPos, String finPos) {
+        System.out.println(String.format("STUB: Valid init=%s fin=%s.", initPos, finPos));
+        return true;
+    }
+
+    private void updateBoard(String initPos, String finPos) {
+        System.out.println(String.format("STUB: Board Updated init=%s fin=%s.", initPos, finPos));
+    }
+}
